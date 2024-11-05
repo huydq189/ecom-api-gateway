8b6014200c14c88b4d6ec78167d2125f95ad4a6e
huydq189 <salt.coffee189@gmail.com>
2024-11-05 21:47:10 +0700

    update source

--- src/auth/dtos/token.dto.ts
+++
@@ -1,9 +0,0 @@
-export class TokenDto {
-  token: string;
-  expires: Date;
-  userId?: number;
-
-  constructor(request: Partial<TokenDto> = {}) {
-    Object.assign(this, request);
-  }
-}
--- test/shared/fixtures/end-to-end.fixture.ts
+++
@@ -1,3 +0,0 @@
-import {IntegrationTestFixture} from "./integration-test.fixture";
-
-export class EndToEndTestFixture extends IntegrationTestFixture {}
